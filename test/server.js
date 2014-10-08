a91d8aa5ed171b322e198a067cb83da67df85514
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-10-07 23:09:15 -0400

    Add slow endpoint for testing timeouts

--- test/server.js
+++ test/server.js
@@ -10,6 +10,12 @@ app.get('/getjson', cors(), function (req, res) {
     res.json({hello: 'world'});
 });
 
+app.get('/slow', cors(), function (req, res) {
+    setTimeout(function () {
+        res.json({hello: 'world'});
+    }, 1000);
+});
+
 // Echo the headers back.
 app.all('/headers', cors(), function (req, res) {
     res.json(req.headers);
